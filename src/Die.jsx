e0b824d3f0d48cbaa0971442f76229b7cf895ecc
Renerd-max <maritine600@gmail.com>
2022-10-24 17:55:39 +0100

    changed the numbers to dots

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,10 +1,72 @@
 import React from "react";
+import ReactDOM from 'react-dom/client'
 
 export default function Dies(props) {
 
    const customStyle = {backgroundColor: props.isHeld? "#59E391": "white"}
 
+   const dotOne = <div id="dotOne" className="dot"></div>
+
+   const dotTwo = <div id="dotTwo">
+    <div className="dot"></div>
+    <div className="dot"></div>
+    </div>
+
+const dotThree = <div id="dotThree">
+    <div className="dot"></div>
+    <div className="dot"></div>
+    <div className="dot"></div>
+    </div>
+
+const dotFour = <div id="dotFour">
+    <div className="dot"></div>
+    <div className="dot"></div>
+    <div className="dot"></div>
+    <div className="dot"></div>
+    </div>
+
+const dotFive = <div id="dotFive">
+    <div id="fiveUp">
+    <div className="dot"></div>
+    <div className="dot"></div>
+    </div>
+
+    <div id="fiveMiddle">
+    <div className="dot"></div>
+    </div>
+
+    <div id="fiveBottom">
+    <div className="dot"></div>
+    <div className="dot"></div>
+    </div>
+    </div>
+
+const dotSix = <div id="dotSix">
+    <div className="dot"></div>
+    <div className="dot"></div>
+    <div className="dot"></div>
+    <div className="dot"></div>
+    <div className="dot"></div>
+    <div className="dot"></div>
+    </div>
+
+   function valueFunct(dot) {
+    if (dot === 1) {
+        return dot = dotOne;
+    } else if(dot === 2) {
+        return dot = dotTwo;
+    } else if(dot === 3) {
+        return dot = dotThree;
+    } else if(dot === 4) {
+        return dot = dotFour;
+    } else if(dot === 5) {
+        return dot = dotFive;
+    } else if(dot === 6) {
+        return dot = dotSix
+    }
+   }
+
     return (
-        <div className="die" style={customStyle} onClick={props.hold} ><p>{props.value}</p></div>
+        <div className="die" style={customStyle} onClick={props.hold} > {valueFunct(props.value)} </div>
     )
 }
